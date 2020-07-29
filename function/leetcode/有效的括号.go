36c7051f9d03eabb9b6160f297e76e492ed0c005
anyingiit <49945850+anyingiit@users.noreply.github.com>
2020-07-29 23:45:30 +0800

    1. 添加了旋转矩阵的解释 2. 添加有效的括号的题解以及解释

---
+++ function/leetcode/有效的括号.go
@@ -0,0 +1,18 @@
+package leetcode
+
+
+func isValid(s string) bool {
+	var stack []uint8
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
+			stack = append(stack, s[i])
+		}else if len(stack) == 0 {
+			return false
+		}else if s[i] == ')' && stack[len(stack)-1] == '(' || s[i] == ']' && stack[len(stack)-1] == '[' || s[i] == '}' && stack[len(stack)-1] == '{'{
+			stack = stack[:len(stack)-1]
+		}else {
+			return false
+		}
+	}
+	return len(stack) == 0
+}
--- main/main.go
+++ main/main.go
@@ -1,9 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"studyGo/function/study"
-)
+import "fmt"
 
 func main(){
 	//fmt.Print("Hello World!")
@@ -11,13 +8,26 @@ func main(){
 	//stack:= study.NewSliceEntry()
 	//stack.Push(1)`
 	//fmt.Println(*stack)
-	mySlice:=study.NewSeliceStack()
-	for i := 0; i < 100; i++ {
-		mySlice.Push(i)
-	}
+	//mySlice:=study.NewSeliceStack()
+	//for i := 0; i < 100; i++ {
+	//	mySlice.Push(i)
+	//}
 	//for i := mySlice.Size(); i > 0; i-- {
 	//	fmt.Print(mySlice.Pop(),"\t\n")
 	//}
-	mySlice.Clear()
-	fmt.Println("is empty?\t",mySlice.IsEmpty())
+	//mySlice.Clear()
+	//fmt.Println("is empty?\t",mySlice.IsEmpty())
+	s:= "abcdefg"
+	var stack []uint8
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
+		stack = append(stack,s[i])
+	}
+	fmt.Println(stack)
+	for i := 0; i < len(stack); i++ {
+		fmt.Print(stack[i],"\t\n")
+	}
+	fmt.Println("---------------")
+	stack = stack[:len(stack)-1]
+	fmt.Println(stack)
 }
